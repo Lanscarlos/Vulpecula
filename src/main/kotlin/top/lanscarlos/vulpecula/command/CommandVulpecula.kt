7f379a641183cf0c741f233f84f8f522c04609c5
Lanscarlos <lanscarlos@hotmail.com>
2022-10-19 17:26:17 +0800

    [2.0.1-2] Add Player to Script Variables

--- src/main/kotlin/top/lanscarlos/vulpecula/command/CommandVulpecula.kt
+++ src/main/kotlin/top/lanscarlos/vulpecula/command/CommandVulpecula.kt
@@ -4,6 +4,8 @@ import org.bukkit.entity.Player
 import taboolib.common.platform.command.CommandBody
 import taboolib.common.platform.command.CommandHeader
 import taboolib.common.platform.command.subCommand
+import taboolib.common.platform.function.info
+import taboolib.module.kether.Kether
 import top.lanscarlos.vulpecula.utils.eval
 
 /**
@@ -20,7 +22,7 @@ object CommandVulpecula {
     val eval = subCommand {
         dynamic {
             execute<Player> { player, _, argument ->
-                val result = eval(argument, player).get()
+                val result = eval(argument, player, args = mapOf("player" to player)).get()
                 player.sendMessage("运行结果：$result")
             }
         }
