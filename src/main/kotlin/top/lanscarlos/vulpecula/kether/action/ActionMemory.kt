f8133b69b7cb79c75387e74d94a5753b110cbc3e
Lanscarlos <lanscarlos@hotmail.com>
2022-11-12 13:28:12 +0800

    [2.0.1-54] Update memory action

--- src/main/kotlin/top/lanscarlos/vulpecula/kether/action/ActionMemory.kt
+++ src/main/kotlin/top/lanscarlos/vulpecula/kether/action/ActionMemory.kt
@@ -1,8 +1,20 @@
 package top.lanscarlos.vulpecula.kether.action
 
-import taboolib.module.kether.actionNow
+import org.bukkit.entity.Entity
+import taboolib.library.kether.ParsedAction
+import taboolib.module.kether.ScriptAction
+import taboolib.module.kether.ScriptFrame
+import taboolib.module.kether.run
 import taboolib.module.kether.scriptParser
+import taboolib.platform.type.BukkitPlayer
 import top.lanscarlos.vulpecula.kether.VulKetherParser
+import top.lanscarlos.vulpecula.kether.live.LiveData
+import top.lanscarlos.vulpecula.kether.live.StringLiveData
+import top.lanscarlos.vulpecula.utils.hasNextToken
+import top.lanscarlos.vulpecula.utils.nextBlock
+import top.lanscarlos.vulpecula.utils.unsafePlayer
+import java.util.concurrent.CompletableFuture
+import java.util.concurrent.ConcurrentHashMap
 
 /**
  * Vulpecula
@@ -11,18 +23,86 @@ import top.lanscarlos.vulpecula.kether.VulKetherParser
  * @author Lanscarlos
  * @since 2022-11-12 00:23
  */
-class ActionMemory {
+class ActionMemory(
+    val key: LiveData<String>,
+    val value: ParsedAction<*>?,
+    val entity: ParsedAction<*>?,
+    val global: Boolean
+) : ScriptAction<Any?>() {
+
+    override fun run(frame: ScriptFrame): CompletableFuture<Any?> {
+        val key = this.key.get(frame, "UNKNOWN_KEY")
+        val entity = if (!global) {
+            this.entity?.let {
+                frame.run(it).join() as? Entity
+            } ?: (frame.unsafePlayer() as? BukkitPlayer)?.player
+        } else null
+
+        return if (this.value != null) {
+            // 设置变量
+            val value = frame.run(this.value).join()
+            setMemory(key, value, entity)
+            CompletableFuture.completedFuture(value)
+        } else {
+            // 获取变量
+            val value = getMemory(key, entity)
+            CompletableFuture.completedFuture(value)
+        }
+    }
 
     companion object {
 
+        private val cache = ConcurrentHashMap<Entity, ConcurrentHashMap<String, Any>>()
+        private val globalCache = ConcurrentHashMap<String, Any>()
+
         /**
+         * 获取变量
+         *
+         * @param entity 对应实体，若为 null 则为全局变量
+         * */
+        fun getMemory(key: String, entity: Entity?): Any? {
+            val map = if (entity != null) {
+                cache.computeIfAbsent(entity) { ConcurrentHashMap() }
+            } else {
+                globalCache
+            }
+            return map[key]
+        }
+
+        /**
+         * 获取变量
+         *
+         * @param value 若为空则删除对应键值
+         * @param entity 对应实体，若为 null 则为全局变量
+         *
+         * @return value 的值
+         * */
+        fun setMemory(key: String, value: Any?, entity: Entity?): Any? {
+            val map = if (entity != null) {
+                cache.computeIfAbsent(entity) { ConcurrentHashMap() }
+            } else {
+                globalCache
+            }
+            if (value != null) {
+                map[key] = value
+            } else {
+                map.remove(key)
+            }
+            return value
+        }
+
+        /**
+         *
+         * 变量是默认相对于实体独立的
          *
          * 获取变量
          * memory {key}
+         * memory {key} by &entity
          * memory {key} -global(g)
          *
          * 设置变量
          * memory {key} to {value}
+         * memory {key} to {value} by &entity
          * memory {key} to {value} -global(g)
          * */
         @VulKetherParser(
@@ -30,7 +110,18 @@ class ActionMemory {
             name = ["memory"]
         )
         fun parser() = scriptParser { reader ->
-            actionNow {  }
+            val key = StringLiveData(reader.nextBlock())
+            val value = if (reader.hasNextToken("to")) {
+                reader.nextBlock()
+            } else null
+
+            val entity = if (reader.hasNextToken("by")) {
+                reader.nextBlock()
+            } else null
+
+            val global = reader.hasNextToken("-global", "global", "-g")
+
+            ActionMemory(key, value, entity, global)
         }
     }
 }
