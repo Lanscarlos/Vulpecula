3f2ba2c0d0385b5c17d82ac439e734e79e1180a2
Lanscarlos <lanscarlos@hotmail.com>
2022-11-13 10:47:58 +0800

    [2.0.1-71] 优化 Sound 语句

--- src/main/kotlin/top/lanscarlos/vulpecula/kether/action/ActionSound.kt
+++ src/main/kotlin/top/lanscarlos/vulpecula/kether/action/ActionSound.kt
@@ -2,7 +2,10 @@ package top.lanscarlos.vulpecula.kether.action
 
 import org.bukkit.Bukkit
 import org.bukkit.Sound
+import org.bukkit.entity.Entity
+import org.bukkit.entity.Player
 import taboolib.common.platform.ProxyPlayer
+import taboolib.common.platform.function.adaptPlayer
 import taboolib.common.util.Location
 import taboolib.library.kether.ParsedAction
 import taboolib.module.kether.*
@@ -23,25 +26,29 @@ import java.util.concurrent.CompletableFuture
 class ActionSound(
     val resource: LiveData<String>,
     val meta: Pair<LiveData<Double>, LiveData<Double>>,
+    val target: LiveData<Entity>?,
     val location: LiveData<Location>?,
     val global: Boolean
 ) : ScriptAction<Any?>() {
 
     override fun run(frame: ScriptFrame): CompletableFuture<Any?> {
 
-        val res = resource.get(frame, "ENTITY_ENDERMAN_DEATH")
+        val res = resource.getOrNull(frame) ?: error("No sound resource selected.")
         val volume = meta.first.get(frame, 1.0).toFloat()
         val pitch = meta.second.get(frame, 1.0).toFloat()
 
         if (global) {
-            val loc = location.getValue(frame, Location("world", 0.0, 0.0, 0.0)).toBukkitLocation()
+            val loc = location?.getOrNull(frame)?.toBukkitLocation() ?: error("No location selected.")
             if (res.startsWith("resource:")) {
                 loc.world?.playSound(loc, res.substring("resource:".length), volume, pitch)
             } else {
                 loc.world?.playSound(loc, Sound.valueOf(res.replace('.', '_').uppercase()), volume, pitch)
             }
         } else {
-            val player = frame.player()
+            val player = target?.getOrNull(frame)?.let {
+                if (it is Player) adaptPlayer(it) else null
+            } ?: frame.unsafePlayer() ?: error("No player selected.")
+
             val loc = location.getValue(frame, player.location)
             if (res.startsWith("resource:")) {
                 player.playSoundResource(loc, res.substring("resource:".length), volume, pitch)
@@ -58,6 +65,7 @@ class ActionSound(
          * sound* {resource}
          * sound* {resource} by {volume} {pitch}
          * sound* {resource} by {volume} {pitch} at {location} global
+         * sound* {resource} by {volume} {pitch} at {location} to {player}
          * */
         @VulKetherParser(
             id = "sound",
@@ -73,14 +81,11 @@ class ActionSound(
             } else {
                 DoubleLiveData(1.0) to DoubleLiveData(1.0)
             }
-            val loc = if (reader.hasNextToken("at")) {
-                reader.readLocation()
-            } else {
-                null
-            }
+            val target = if (reader.hasNextToken("to")) reader.readEntity() else null
+            val loc = if (reader.hasNextToken("at")) reader.readLocation() else null
             val global = reader.hasNextToken("global", "-global", "--global")
 
-            ActionSound(resource, meta, loc, global)
+            ActionSound(resource, meta, target, loc, global)
         }
     }
 }
