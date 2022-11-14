4d88559c0dd1b3885c0e9e2f2a223d5753f20534
Lanscarlos <lanscarlos@hotmail.com>
2022-11-14 16:20:51 +0800

    [2.0.2-3] update config util

--- src/main/kotlin/top/lanscarlos/vulpecula/utils/ConfigUtil.kt
+++ src/main/kotlin/top/lanscarlos/vulpecula/utils/ConfigUtil.kt
@@ -1,6 +1,7 @@
 package top.lanscarlos.vulpecula.utils
 
 import taboolib.library.configuration.ConfigurationSection
+import taboolib.module.configuration.Configuration
 
 /**
  * Vulpecula
@@ -15,5 +16,32 @@ fun ConfigurationSection.getStringOrList(path: String): List<String> {
         is String -> listOf(it)
         is List<*> -> it.mapNotNull { it?.toString() }
         else -> emptyList()
+    }
+}
+
+/**
+ * 加载 Config 下所有 String 内容
+ * */
+inline fun ConfigurationSection.forEachLine(func: ((key: String, value: String) -> Unit)) {
+    getKeys(false).forEach { key ->
+        getString(key)?.let { func(key, it) }
+    }
+}
+
+inline fun <reified T> Configuration.forEachObject(func: ((key: String, value: T) -> Unit)) {
+    getKeys(false).forEach { key ->
+        val it = get(key)
+        if (it is T) func(key, it)
+    }
+}
+
+/**
+ * 加载 Config 下所有 Section 内容
+ * */
+inline fun Configuration.forEachSections(func: ((key: String, section: ConfigurationSection) -> Unit)) {
+    getKeys(false).forEach { key ->
+        getConfigurationSection(key)?.let { section ->
+            func(key, section)
+        }
     }
 }
--- src/main/kotlin/top/lanscarlos/vulpecula/utils/FileUtil.kt
+++ src/main/kotlin/top/lanscarlos/vulpecula/utils/FileUtil.kt
@@ -34,40 +34,6 @@ fun File.toConfig(): Configuration {
     return Configuration.loadFromFile(this)
 }
 
-/**
- * 加载 Config 下所有 String 内容
- * */
-inline fun ConfigurationSection.forEachLine(func: ((key: String, value: String) -> Unit)) {
-    getKeys(false).forEach { key ->
-        getString(key)?.let { func(key, it) }
-    }
-}
-
-inline fun <reified T> Configuration.forEachObject(func: ((key: String, value: T) -> Unit)) {
-    getKeys(false).forEach { key ->
-        val it = get(key)
-        if (it is T) func(key, it)
-    }
-}
-
-/**
- * 加载 Config 下所有 Section 内容
- * */
-inline fun Configuration.forEachSections(func: ((key: String, section: ConfigurationSection) -> Unit)) {
-    getKeys(false).forEach { key ->
-        getConfigurationSection(key)?.let { section ->
-            func(key, section)
-        }
-    }
-}
-
-/**
- * 加载该目录下所有子文件的 Section 内容
- * */
-inline fun File.deepSections(crossinline func: ((file: File, key: String, section: ConfigurationSection) -> Unit)) {
-    this.getFiles().forEach { it.toConfig().forEachSections { key, section -> func(it, key, section) } }
-}
-
 inline fun File.addWatcher(runFirst: Boolean = false, crossinline func: (File.() -> Unit)): File {
     if (FileWatcher.INSTANCE.hasListener(this)) return this
     FileWatcher.INSTANCE.addSimpleListener(this, { func(this) }, runFirst)
