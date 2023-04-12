e21a863a8ad6230f31b7d083a62e3bac4cb649de
Lanscarlos <lanscarlos@hotmail.com>
2023-04-12 09:31:12 +0800

    [2.2.0-7] 新增 Inventory Find 语句

---
+++ src/main/kotlin/top/lanscarlos/vulpecula/bacikal/action/inventory/ActionInventoryFind.kt
@@ -0,0 +1,30 @@
+package top.lanscarlos.vulpecula.bacikal.action.inventory
+
+import top.lanscarlos.vulpecula.utils.chemdah.InferItem.Companion.toInferItem
+
+/**
+ * Vulpecula
+ * top.lanscarlos.vulpecula.bacikal.action.inventory
+ *
+ * @author Lanscarlos
+ * @since 2023-04-12 09:13
+ */
+object ActionInventoryFind : ActionInventory.Resolver {
+
+    override val name: Array<String> = arrayOf("find")
+
+    override fun resolve(reader: ActionInventory.Reader): ActionInventory.Handler<out Any?> {
+        return reader.handle {
+            combine(
+                source(),
+                text("pattern"),
+            ) { inventory, pattern ->
+                val infer = pattern.toInferItem()
+                for ((index, item) in inventory.withIndex()) {
+                    if (infer.match(item)) return@combine index
+                }
+                return@combine -1
+            }
+        }
+    }
+}
