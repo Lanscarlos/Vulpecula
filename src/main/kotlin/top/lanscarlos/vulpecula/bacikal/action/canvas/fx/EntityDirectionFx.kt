d933c7d0ce97a2124d8c0cbdd685fa5cb730f4a5
Lanscarlos <lanscarlos@hotmail.com>
2023-07-08 13:48:30 +0800

    [2.3.2-3] update Fx Entity

--- src/main/kotlin/top/lanscarlos/vulpecula/bacikal/action/canvas/fx/EntityDirectionFx.kt
+++ src/main/kotlin/top/lanscarlos/vulpecula/bacikal/action/canvas/fx/EntityDirectionFx.kt
@@ -2,6 +2,10 @@ package top.lanscarlos.vulpecula.bacikal.action.canvas.fx
 
 import org.bukkit.entity.Entity
 import taboolib.common.util.Vector
+import top.lanscarlos.vulpecula.bacikal.Bacikal
+import top.lanscarlos.vulpecula.bacikal.BacikalReader
+import top.lanscarlos.vulpecula.utils.playerOrNull
+import top.lanscarlos.vulpecula.utils.toBukkit
 
 /**
  * Vulpecula
@@ -12,18 +16,42 @@ import taboolib.common.util.Vector
  * @author Lanscarlos
  * @since 2023-06-30 01:37
  */
-class EntityDirectionFx(val entity: Entity) : VectorFx<Entity>() {
+class EntityDirectionFx(val entity: Entity, val normalize: Boolean, val ignoreVertical: Boolean) : VectorFx<Entity>() {
 
     override fun calculate(): Vector {
         return calculate(entity)
     }
 
     override fun calculate(input: Entity): Vector {
-        return input.location.direction.let { Vector(it.x, it.y, it.z) }
+        val direction = input.location.direction.let {
+            Vector(it.x, if (ignoreVertical) 0.0 else it.y, it.z)
+        }
+        return if (normalize) direction.normalize() else direction
     }
 
     override fun copy(): Fx<Entity, Vector> {
         // 无需克隆
         return this
+    }
+
+    companion object : ActionFx.Resolver {
+
+        override val name = arrayOf("entity-direction", "entity-face")
+
+        override fun resolve(reader: BacikalReader): Bacikal.Parser<Fx<*, *>> {
+            return reader.run {
+                combine(
+                    argument("bind", "entity", then = entity(display = "fx entity-direction entity")),
+                    argument("normalize", "normal", then = bool(display = "fx entity-direction normalize"), def = false),
+                    argument("igv", then = bool(display = "fx entity-direction igv"), def = false)
+                ) { entity, normalize, ignoreVertical ->
+                    EntityDirectionFx(
+                        entity ?: this.playerOrNull()?.toBukkit() ?: error("No fx entity-direction entity found."),
+                        normalize,
+                        ignoreVertical
+                    )
+                }
+            }
+        }
     }
 }
---
+++ src/main/kotlin/top/lanscarlos/vulpecula/bacikal/action/canvas/fx/EntityPitchFx.kt
@@ -0,0 +1,51 @@
+package top.lanscarlos.vulpecula.bacikal.action.canvas.fx
+
+import org.bukkit.entity.Entity
+import org.bukkit.entity.Player
+import top.lanscarlos.vulpecula.bacikal.Bacikal
+import top.lanscarlos.vulpecula.bacikal.BacikalReader
+import top.lanscarlos.vulpecula.utils.playerOrNull
+import top.lanscarlos.vulpecula.utils.toBukkit
+
+/**
+ * Vulpecula
+ * top.lanscarlos.vulpecula.bacikal.action.canvas.fx
+ *
+ * @author Lanscarlos
+ * @since 2023-07-08 13:31
+ */
+class EntityPitchFx(val entity: Entity) : DecimalFx<Entity>() {
+
+    override fun calculate(): Double {
+        return calculate(entity)
+    }
+
+    override fun calculate(input: Entity): Double {
+        return if (entity is Player) {
+            entity.eyeLocation.pitch.toDouble()
+        } else {
+            entity.location.pitch.toDouble()
+        }
+    }
+
+    override fun copy(): EntityPitchFx {
+        return EntityPitchFx(entity)
+    }
+
+    companion object : ActionFx.Resolver {
+
+        override val name = arrayOf("entity-pitch", "pitch")
+
+        override fun resolve(reader: BacikalReader): Bacikal.Parser<Fx<*, *>> {
+            return reader.run {
+                combine(
+                    argument("bind", "entity", then = entity(display = "fx entity-pitch entity"))
+                ) { entity ->
+                    EntityPitchFx(
+                        entity ?: this.playerOrNull()?.toBukkit() ?: error("No fx entity-pitch entity found.")
+                    )
+                }
+            }
+        }
+    }
+}
