d80625f464c6ac5aec595472154126e93710d5f4
Lanscarlos <lanscarlos@hotmail.com>
2023-08-18 16:44:44 +0800

    [experimental] fix volatile 17.

--- project/common-volatile-17/build.gradle.kts
+++ project/common-volatile-17/build.gradle.kts
@@ -1,3 +1,8 @@
 dependencies {
     compileOnly("ink.ptms.core:v12000:12000:mapped")
 }
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
