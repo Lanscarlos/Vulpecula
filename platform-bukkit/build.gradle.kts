eb98d091d77ddf79bf62de0f2b5d45cf478020fd
Lanscarlos <lanscarlos@hotmail.com>
2025-04-27 17:16:17 +0800

    update.

---
+++ platform-bukkit/build.gradle.kts
@@ -0,0 +1,4 @@
+
+taboolib {
+    subproject = true
+}
---
+++ platform-bukkit/src/main/kotlin/top/lanscarlos/vulpecula/platform/bukkit/VulpeculaPlugin.kt
@@ -0,0 +1,19 @@
+package top.lanscarlos.vulpecula.platform.bukkit
+
+import taboolib.common.platform.Plugin
+import taboolib.common.platform.function.info
+
+/**
+ * Vulpecula
+ * top.lanscarlos.vulpecula.platform.bukkit
+ *
+ * @author Lanscarlos
+ * @since 2025/4/27 17:15
+ */
+object VulpeculaPlugin : Plugin() {
+
+    override fun onEnable() {
+        info("Successfully running Vulpecula!")
+    }
+
+}
