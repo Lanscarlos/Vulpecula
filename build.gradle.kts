f6893d3658567b0dd23ad6f9f0d50c9dcbeb6658
Lanscarlos <lanscarlos@hotmail.com>
2025-04-28 09:27:19 +0800

    更新 TabooLib 版本

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ import io.izzel.taboolib.gradle.Kether
 
 plugins {
     java
-    id("io.izzel.taboolib") version "2.0.22"
+    id("io.izzel.taboolib") version "2.0.23"
     id("org.jetbrains.kotlin.jvm") version "2.0.21"
 }
 
@@ -34,7 +34,7 @@ subprojects {
             install(Kether)
         }
         version {
-            taboolib = "6.2.0"
+            taboolib = "6.2.3-8cc2f66"
         }
         description {
             name(rootProject.name)
