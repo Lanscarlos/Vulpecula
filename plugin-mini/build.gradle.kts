aa96e9c35117b4b12bd1605a70e1eb90574bb6d3
Lanscarlos <lanscarlos@hotmail.com>
2025-04-28 09:30:03 +0800

    更新构建配置

--- plugin-mini/build.gradle.kts
+++ plugin-mini/build.gradle.kts
@@ -1,23 +1,6 @@
 
 taboolib {
     subproject = false
-    description {
-        name(rootProject.name)
-        contributors {
-            name("Lanscarlos")
-        }
-        dependencies {
-            name("Adyeshach").optional(true)
-            name("Chemdah").optional(true)
-            name("DungeonPlus").optional(true)
-            name("Planners").optional(true)
-            name("Invero").optional(true)
-            name("Zaphkiel").optional(true)
-
-            name("PlaceholderAPI").optional(true)
-            name("LuckPerms").optional(true)
-        }
-    }
     relocate("kotlinx.metadata.", "kotlinx.metadata060.")
 }
 
