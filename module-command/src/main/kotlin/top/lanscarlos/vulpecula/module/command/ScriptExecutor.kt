c348c2d6a4ed87c91e9b6e23e80124371f083f60
Lanscarlos <lanscarlos@hotmail.com>
2025-05-12 13:59:49 +0800

    修复编译错误.

--- module-command/src/main/kotlin/top/lanscarlos/vulpecula/module/command/ScriptExecutor.kt
+++ module-command/src/main/kotlin/top/lanscarlos/vulpecula/module/command/ScriptExecutor.kt
@@ -111,10 +111,10 @@ class ScriptExecutor(
     ): CompletableFuture<Any?> {
         return when (script) {
             is String -> {
-                ScriptService.run(script, sender, args, onSuccess = onSuccess, onFailure = onFailure)
+                ScriptService.run(script, sender, args, onSuccess = onSuccess, onFailure = onFailure).future
             }
             is Script -> {
-                ScriptService.run(script, sender, args, onSuccess = onSuccess, onFailure = onFailure)
+                ScriptService.run(script, sender, args, onSuccess = onSuccess, onFailure = onFailure).future
             }
             else -> error("Unsupported script type: ${script.javaClass.name}")
         }
