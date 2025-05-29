383aef5438c1d10eb899ff449f379116ea075afc
Lanscarlos <lanscarlos@hotmail.com>
2025-05-29 14:12:22 +0800

    formatted.

--- common-applicative/src/main/kotlin/top/lanscarlos/vulpecula/common/applicative/LocationApplicative.kt
+++ common-applicative/src/main/kotlin/top/lanscarlos/vulpecula/common/applicative/LocationApplicative.kt
@@ -1,6 +1,5 @@
 package top.lanscarlos.vulpecula.common.applicative
 
-import org.bukkit.Bukkit
 import taboolib.common.util.Location
 import org.bukkit.entity.Entity
 import org.bukkit.entity.Player
