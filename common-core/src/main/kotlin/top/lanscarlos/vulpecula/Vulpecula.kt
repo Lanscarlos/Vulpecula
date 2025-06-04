e69cc323342acfbc156930a58dfb44e54680b284
Lanscarlos <lanscarlos@hotmail.com>
2025-06-04 10:24:41 +0800

    移除冗余内容.

--- common-core/src/main/kotlin/top/lanscarlos/vulpecula/Vulpecula.kt
+++ common-core/src/main/kotlin/top/lanscarlos/vulpecula/Vulpecula.kt
@@ -1,20 +1,5 @@
 package top.lanscarlos.vulpecula
 
-import org.bukkit.event.entity.EntityDamageEvent
-import org.bukkit.event.entity.EntityDeathEvent
-import org.bukkit.event.entity.EntitySpawnEvent
-import org.bukkit.event.entity.ItemDespawnEvent
-import org.bukkit.event.player.PlayerDropItemEvent
-import taboolib.common.LifeCycle
-import taboolib.common.platform.Awake
-import taboolib.common.platform.ProxyCommandSender
-import taboolib.common.platform.command.CommandBody
-import taboolib.common.platform.command.subCommand
-import taboolib.common.platform.event.SubscribeEvent
-import taboolib.common.platform.function.info
-import taboolib.module.configuration.Config
-import taboolib.module.configuration.Configuration
-
 /**
  * Vulpecula
  * top.lanscarlos.vulpecula
