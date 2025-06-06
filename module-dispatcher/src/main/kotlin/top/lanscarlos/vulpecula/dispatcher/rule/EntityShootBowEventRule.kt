b29fd54035a62a8eb444bb49881e2e97aa2c7ecf
Lanscarlos <lanscarlos@hotmail.com>
2025-06-06 22:43:19 +0800

    feat: 新增 EntityShootBowEventRule 和 EntityEventRule 以支持实体事件的规则解析和变量提取

---
+++ module-dispatcher/src/main/kotlin/top/lanscarlos/vulpecula/dispatcher/rule/EntityShootBowEventRule.kt
@@ -0,0 +1,25 @@
+package top.lanscarlos.vulpecula.dispatcher.rule
+
+import org.bukkit.event.entity.EntityShootBowEvent
+import taboolib.library.configuration.ConfigurationSection
+import taboolib.library.reflex.ReflexClass
+
+/**
+ * Vulpecula
+ * top.lanscarlos.vulpecula.dispatcher.rule
+ *
+ * @author Lanscarlos
+ * @since 2025/6/6 22:39
+ */
+class EntityShootBowEventRule(clazz: ReflexClass, config: ConfigurationSection) : EntityEventRule<EntityShootBowEvent>(clazz, config) {
+
+    override fun parseVariables(event: EntityShootBowEvent): Map<String, Any?> {
+        return super.parseVariables(event).plus(
+            mapOf(
+                "projectile" to event.projectile,
+                "bow" to event.bow,
+                "force" to event.force
+            )
+        )
+    }
+}
