6e9e241b366f4ce88fe2b28ebd3d58190c353464
Lanscarlos <lanscarlos@hotmail.com>
2025-06-07 13:33:48 +0800

    refactor: 优化时间解析逻辑并新增毫秒单位支持以提升功能完整性与代码可维护性

--- common-core/src/main/kotlin/top/lanscarlos/vulpecula/utils/TimeUtil.kt
+++ common-core/src/main/kotlin/top/lanscarlos/vulpecula/utils/TimeUtil.kt
@@ -13,11 +13,12 @@ import top.lanscarlos.vulpecula.common.core.exception.InvalidTimeUnitException
 object TimeUtil {
 
     fun parse(value: String): Long {
-        val regex = Regex("^(\\d+)(ticks|tick|t|seconds|second|s|minutes|minute|min|m|hours|hour|h)$", RegexOption.IGNORE_CASE)
+        val regex = Regex("^(\\d+)(ticks|tick|t|ms|seconds|second|s|minutes|minute|min|m|hours|hour|h)$", RegexOption.IGNORE_CASE)
         val matches = regex.find(value) ?: throw InvalidTimeFormatException(value)
         val time = matches.groupValues[1].toLong()
         return when (val unit = matches.groupValues[2].lowercase()) {
             "ticks", "tick", "t" -> time * 50
+            "ms" -> time
             "seconds", "second", "s" -> time * 1_000
             "minutes", "minute", "min", "m" -> time * 60_000
             "hours", "hour", "h" -> time * 3_600_000
