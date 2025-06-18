706547608cecd61b6683dec08acc146aac4edf68
Lanscarlos <lanscarlos@hotmail.com>
2025-06-18 21:04:30 +0800

    feat: 增加 event ignore 语句

---
+++ module-action-event/src/main/kotlin/top/lanscarlos/vulpecula/module/action/event/EventIgnoreAction.kt
@@ -0,0 +1,21 @@
+package top.lanscarlos.vulpecula.module.action.event
+
+import top.lanscarlos.vulpecula.module.bacikal.annotation.BacikalParser
+import top.lanscarlos.vulpecula.module.bacikal.parser.BacikalFrame
+import top.lanscarlos.vulpecula.module.bacikal.parser.ClassActionResolver
+
+/**
+ * Vulpecula
+ * top.lanscarlos.vulpecula.module.action.event
+ *
+ * @author Lanscarlos
+ * @since 2025/6/18
+ */
+@BacikalParser("event.ignore")
+object EventIgnoreAction : ClassActionResolver {
+
+    fun resolve(frame: BacikalFrame) {
+        frame.setVariable("@EVENT_STATUS", "IGNORED")
+    }
+
+}
