cb32fa92ddc4a57d28fe0757dfdf26cf9160e1f0
Lanscarlos <lanscarlos@hotmail.com>
2025-06-29 22:58:48 +0800

    feat: 添加 ActionItemAmount 类并实现相关数量操作方法

---
+++ module-action-item/src/main/kotlin/top/lanscarlos/vulpecula/module/action/item/ActionItemAmount.kt
@@ -0,0 +1,66 @@
+package top.lanscarlos.vulpecula.module.action.item
+
+import top.lanscarlos.vulpecula.common.core.utils.asLang
+import top.lanscarlos.vulpecula.module.bacikal.annotation.BacikalParser
+import top.lanscarlos.vulpecula.module.bacikal.parser.BacikalFrame
+import top.lanscarlos.vulpecula.module.bacikal.parser.ClassActionResolver
+
+/**
+ * Vulpecula
+ * top.lanscarlos.vulpecula.module.action.item
+ *
+ * @author Lanscarlos
+ * @since 2025/6/29
+ */
+@BacikalParser("item.amount.maximum", aliases = ["max"])
+object ActionItemAmountMaximum: ClassActionResolver {
+
+    fun resolve(frame: BacikalFrame): Int {
+        val item = ActionItem.getContext(frame)
+        return item.type.maxStackSize
+    }
+
+}
+
+@BacikalParser("item.amount.get")
+object ActionItemAmountGet : ClassActionResolver {
+
+    fun resolve(frame: BacikalFrame): Int {
+        val item = ActionItem.getContext(frame)
+        return item.amount
+    }
+
+}
+
+@BacikalParser("item.amount.set")
+object ActionItemAmountSet : ClassActionResolver {
+
+    fun resolve(frame: BacikalFrame, amount: Int) {
+        val item = ActionItem.getContext(frame)
+        require(amount in 1..item.type.maxStackSize) {
+            asLang("module-action-item-exception-invalid-amount", amount)
+        }
+        item.amount = amount
+    }
+
+}
+
+@BacikalParser("item.amount.increase", aliases = ["inc", "add"])
+object ActionItemAmountIncrease : ClassActionResolver {
+
+    fun resolve(frame: BacikalFrame, amount: Int) {
+        val item = ActionItem.getContext(frame)
+        item.amount = (item.amount + amount).coerceAtMost(item.type.maxStackSize)
+    }
+
+}
+
+@BacikalParser("item.amount.decrease", aliases = ["dec"])
+object ActionItemAmountDecrease : ClassActionResolver {
+
+    fun resolve(frame: BacikalFrame, amount: Int) {
+        val item = ActionItem.getContext(frame)
+        item.amount = (item.amount - amount).coerceAtLeast(1)
+    }
+
+}
