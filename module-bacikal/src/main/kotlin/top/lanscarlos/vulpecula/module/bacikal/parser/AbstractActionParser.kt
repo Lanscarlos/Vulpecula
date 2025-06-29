e91c909b769bc479528e97fb07826685291b36d8
Lanscarlos <lanscarlos@hotmail.com>
2025-06-29 16:52:45 +0800

    refactor: Remove unused import for StandardColors

--- module-bacikal/src/main/kotlin/top/lanscarlos/vulpecula/module/bacikal/parser/AbstractActionParser.kt
+++ module-bacikal/src/main/kotlin/top/lanscarlos/vulpecula/module/bacikal/parser/AbstractActionParser.kt
@@ -2,7 +2,6 @@ package top.lanscarlos.vulpecula.module.bacikal.parser
 
 import taboolib.module.chat.ComponentText
 import taboolib.module.chat.Components
-import taboolib.module.chat.StandardColors
 
 /**
  * Vulpecula
