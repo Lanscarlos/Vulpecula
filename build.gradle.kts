5059cf1ecb38ac6cc68bcb88a3a180a6a4e76bce
Lanscarlos <lanscarlos@hotmail.com>
2025-07-01 11:39:55 +0800

    chore: 添加并调整部分模块安装顺序

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,14 +24,16 @@ subprojects {
             // 安装模块
             install(Basic)
             install(Bukkit)
+            install(BukkitNMS)
+            install(BukkitNMSUtil)
             install(BukkitUI)
             install(BukkitUtil)
-            install(BukkitNMSUtil)
             install(CommandHelper)
             install(I18n)
+            install(JavaScript)
+            install(Kether)
             install(Metrics)
             install(MinecraftChat)
-            install(Kether)
         }
         version {
             taboolib = "6.2.3-8cc2f66"
