a68ef2f38ae017262dee398e9aeebdab3bfcd974
Lanscarlos <lanscarlos@hotmail.com>
2025-07-02 20:58:06 +0800

    refactor: 优化 `getEnchantment` 方法并引入 `enchantmentMap`

--- module-action-item/src/main/kotlin/top/lanscarlos/vulpecula/module/action/item/ActionItemEnchantment.kt
+++ module-action-item/src/main/kotlin/top/lanscarlos/vulpecula/module/action/item/ActionItemEnchantment.kt
@@ -91,13 +91,48 @@ object ActionItemEnchantmentClear : ClassActionResolver {
 
 }
 
-private fun getEnchantment(value: String): Enchantment {
-    val iterator = Registry.ENCHANTMENT.iterator()
-    while (iterator.hasNext()) {
-        val enchantment = iterator.next()
-        if (enchantment.key.key.equals(value, true)) {
-            return enchantment
-        }
-    }
-    error(asLang("module-action-item-exception-invalid-enchantment-type", value))
+private fun getEnchantment(enchantment: String): Enchantment {
+    return enchantmentMap[enchantment.uppercase()] ?: error(asLang("module-action-item-exception-invalid-enchantment-type", enchantment))
 }
+
+private val enchantmentMap: Map<String, Enchantment> = mapOf(
+    "PROTECTION_ENVIRONMENTAL" to Enchantment.PROTECTION_ENVIRONMENTAL,
+    "PROTECTION_FIRE" to Enchantment.PROTECTION_FIRE,
+    "PROTECTION_FALL" to Enchantment.PROTECTION_FALL,
+    "PROTECTION_EXPLOSIONS" to Enchantment.PROTECTION_EXPLOSIONS,
+    "PROTECTION_PROJECTILE" to Enchantment.PROTECTION_PROJECTILE,
+    "OXYGEN" to Enchantment.OXYGEN,
+    "WATER_WORKER" to Enchantment.WATER_WORKER,
+    "THORNS" to Enchantment.THORNS,
+    "DEPTH_STRIDER" to Enchantment.DEPTH_STRIDER,
+    "FROST_WALKER" to Enchantment.FROST_WALKER,
+    "BINDING_CURSE" to Enchantment.BINDING_CURSE,
+    "DAMAGE_ALL" to Enchantment.DAMAGE_ALL,
+    "DAMAGE_UNDEAD" to Enchantment.DAMAGE_UNDEAD,
+    "DAMAGE_ARTHROPODS" to Enchantment.DAMAGE_ARTHROPODS,
+    "KNOCKBACK" to Enchantment.KNOCKBACK,
+    "FIRE_ASPECT" to Enchantment.FIRE_ASPECT,
+    "LOOT_BONUS_MOBS" to Enchantment.LOOT_BONUS_MOBS,
+    "SWEEPING_EDGE" to Enchantment.SWEEPING_EDGE,
+    "DIG_SPEED" to Enchantment.DIG_SPEED,
+    "SILK_TOUCH" to Enchantment.SILK_TOUCH,
+    "DURABILITY" to Enchantment.DURABILITY,
+    "LOOT_BONUS_BLOCKS" to Enchantment.LOOT_BONUS_BLOCKS,
+    "ARROW_DAMAGE" to Enchantment.ARROW_DAMAGE,
+    "ARROW_KNOCKBACK" to Enchantment.ARROW_KNOCKBACK,
+    "ARROW_FIRE" to Enchantment.ARROW_FIRE,
+    "ARROW_INFINITE" to Enchantment.ARROW_INFINITE,
+    "LUCK" to Enchantment.LUCK,
+    "LURE" to Enchantment.LURE,
+    "LOYALTY" to Enchantment.LOYALTY,
+    "IMPALING" to Enchantment.IMPALING,
+    "RIPTIDE" to Enchantment.RIPTIDE,
+    "CHANNELING" to Enchantment.CHANNELING,
+    "MULTISHOT" to Enchantment.MULTISHOT,
+    "QUICK_CHARGE" to Enchantment.QUICK_CHARGE,
+    "PIERCING" to Enchantment.PIERCING,
+    "MENDING" to Enchantment.MENDING,
+    "VANISHING_CURSE" to Enchantment.VANISHING_CURSE,
+    "SOUL_SPEED" to Enchantment.SOUL_SPEED,
+    "SWIFT_SNEAK" to Enchantment.SWIFT_SNEAK
+)
