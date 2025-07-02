a25b985084e0b7e0f1392824105a7b90e3c7749c
Lanscarlos <lanscarlos@hotmail.com>
2025-07-02 13:39:15 +0800

    chore: 排除 Action 中的 config.yml 文件

--- workflow-resources/build.gradle.kts
+++ workflow-resources/build.gradle.kts
@@ -19,6 +19,10 @@ for (project in rootProject.subprojects.filter { it.depth == 1 && it.name.starts
             for (dependency in dependencies) {
                 for (file in files(dependency.sourceSets["main"].resources)) {
                     val name = file.absolutePath.substringAfter("resources\\")
+                    if (dependency.name.startsWith("module-action-") && name == "config.yml") {
+                        // 排除 Action 中的 config.yml
+                        continue
+                    }
                     val resource = resources.computeIfAbsent(name) { File(workspace, name) }
                     if (!resource.exists()) {
                         resource.parentFile.mkdirs()
