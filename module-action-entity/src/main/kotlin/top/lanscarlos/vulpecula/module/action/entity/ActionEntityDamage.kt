2da8cb1d6635d27432ac3325a01b015c7e7933df
Lanscarlos <lanscarlos@hotmail.com>
2025-07-03 21:09:55 +0800

    feat: 添加实体伤害和传送功能

---
+++ module-action-entity/src/main/kotlin/top/lanscarlos/vulpecula/module/action/entity/ActionEntityDamage.kt
@@ -0,0 +1,33 @@
+package top.lanscarlos.vulpecula.module.action.entity
+
+import org.bukkit.entity.Damageable
+import org.bukkit.entity.Entity
+import top.lanscarlos.vulpecula.common.core.utils.asLang
+import top.lanscarlos.vulpecula.module.bacikal.annotation.BacikalParser
+import top.lanscarlos.vulpecula.module.bacikal.annotation.Optional
+import top.lanscarlos.vulpecula.module.bacikal.parser.BacikalFrame
+import top.lanscarlos.vulpecula.module.bacikal.parser.ClassActionResolver
+
+/**
+ * Vulpecula
+ * top.lanscarlos.vulpecula.module.action.entity
+ *
+ * @author Lanscarlos
+ * @since 2025/7/3
+ */
+@BacikalParser("entity.damage")
+object ActionEntityDamage : ClassActionResolver {
+
+    fun resolve(
+        frame: BacikalFrame,
+        damage: Double,
+        @Optional(["by"]) damager: Entity? = null
+    ) {
+        val entity = ActionEntity.getContext(frame)
+        require(entity is Damageable) {
+            asLang("module-action-entity-exception-entity-unsupported-damage", entity.type.name)
+        }
+        entity.damage(damage, damager)
+    }
+
+}
---
+++ module-action-entity/src/main/kotlin/top/lanscarlos/vulpecula/module/action/entity/ActionEntityTeleport.kt
@@ -0,0 +1,25 @@
+package top.lanscarlos.vulpecula.module.action.entity
+
+import taboolib.common.util.Location
+import taboolib.platform.util.toBukkitLocation
+import top.lanscarlos.vulpecula.module.bacikal.annotation.BacikalParser
+import top.lanscarlos.vulpecula.module.bacikal.annotation.Expected
+import top.lanscarlos.vulpecula.module.bacikal.parser.BacikalFrame
+import top.lanscarlos.vulpecula.module.bacikal.parser.ClassActionResolver
+
+/**
+ * Vulpecula
+ * top.lanscarlos.vulpecula.module.action.entity
+ *
+ * @author Lanscarlos
+ * @since 2025/7/3
+ */
+@BacikalParser("entity.teleport")
+object ActionEntityTeleport : ClassActionResolver {
+
+    fun resolve(frame: BacikalFrame, @Expected(["to"]) location: Location) {
+        val entity = ActionEntity.getContext(frame)
+        entity.teleport(location.toBukkitLocation())
+    }
+
+}
