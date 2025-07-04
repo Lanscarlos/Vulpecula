c062050eca153fbcf9955a18f0e8a87caa37c6e3
Lanscarlos <lanscarlos@hotmail.com>
2025-07-04 09:54:55 +0800

    feat: 添加开发信息命令以显示插件版本

---
+++ module-core/src/main/kotlin/top/lanscarlos/vulpecula/module/core/command/DevelopInfoCommand.kt
@@ -0,0 +1,24 @@
+package top.lanscarlos.vulpecula.module.core.command
+
+import taboolib.common.platform.ProxyCommandSender
+import taboolib.common.platform.command.CommandBody
+import taboolib.common.platform.command.subCommand
+import taboolib.common.platform.function.pluginVersion
+
+/**
+ * Vulpecula
+ * top.lanscarlos.vulpecula.module.core.command
+ *
+ * @author Lanscarlos
+ * @since 2025/7/4 9:04
+ */
+object DevelopInfoCommand {
+
+    @CommandBody(["@DEVELOP"])
+    val version = subCommand {
+        execute<ProxyCommandSender> { sender, _, _ ->
+            sender.sendMessage("Vulpecula Version: $pluginVersion")
+        }
+    }
+
+}
