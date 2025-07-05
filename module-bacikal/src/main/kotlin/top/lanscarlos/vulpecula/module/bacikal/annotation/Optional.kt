2d50ccd27620a3fc127b0c47c8abb3b5b9a09d1e
Lanscarlos <lanscarlos@hotmail.com>
2025-07-05 21:28:43 +0800

    feat: 添加注解的目标和保留策略

--- module-bacikal/src/main/kotlin/top/lanscarlos/vulpecula/module/bacikal/annotation/Optional.kt
+++ module-bacikal/src/main/kotlin/top/lanscarlos/vulpecula/module/bacikal/annotation/Optional.kt
@@ -7,4 +7,6 @@ package top.lanscarlos.vulpecula.module.bacikal.annotation
  * @author Lanscarlos
  * @since 2024-11-20 11:07
  */
+@Target(AnnotationTarget.VALUE_PARAMETER)
+@Retention(AnnotationRetention.RUNTIME)
 annotation class Optional(val values: Array<String>)
