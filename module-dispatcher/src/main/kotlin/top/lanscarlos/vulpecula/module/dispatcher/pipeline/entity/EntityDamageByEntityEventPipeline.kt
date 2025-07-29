c0d24fe85951d4776f94acd60aa6aee12296148b
Lanscarlos <lanscarlos@hotmail.com>
2025-07-29 22:28:12 +0800

    feat: 增加 EntityDamageByEntityEvent 事件流支持

---
+++ module-dispatcher/src/main/kotlin/top/lanscarlos/vulpecula/module/dispatcher/pipeline/entity/EntityDamageByEntityEventPipeline.kt
@@ -0,0 +1,24 @@
+package top.lanscarlos.vulpecula.module.dispatcher.pipeline.entity
+
+import org.bukkit.event.entity.EntityDamageByEntityEvent
+import taboolib.library.configuration.ConfigurationSection
+import top.lanscarlos.vulpecula.module.dispatcher.Context
+import top.lanscarlos.vulpecula.module.dispatcher.pipeline.AbstractPipeline
+import top.lanscarlos.vulpecula.module.dispatcher.pipeline.AutoRegistered
+
+/**
+ * Vulpecula
+ * top.lanscarlos.vulpecula.module.dispatcher.pipeline.entity
+ *
+ * @author Lanscarlos
+ * @since 2025/7/29
+ */
+@AutoRegistered
+class EntityDamageByEntityEventPipeline(clazz: Class<*>, config: ConfigurationSection) : AbstractPipeline<EntityDamageByEntityEvent>(clazz, config) {
+
+    override fun initVariables(context: Context) {
+        val event = getEvent(context)
+        context.setVariable("damager", event.damager)
+    }
+
+}
