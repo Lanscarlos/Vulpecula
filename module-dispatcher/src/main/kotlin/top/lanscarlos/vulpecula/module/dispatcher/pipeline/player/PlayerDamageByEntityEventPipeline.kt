308f8c8c114ad3463f955690f7c3ea9a43687f42
Lanscarlos <lanscarlos@hotmail.com>
2025-07-29 22:58:56 +0800

    feat: 增加 PlayerDamageByEntityEvent 事件流支持

---
+++ module-dispatcher/src/main/kotlin/top/lanscarlos/vulpecula/module/dispatcher/pipeline/player/PlayerDamageByEntityEventPipeline.kt
@@ -0,0 +1,15 @@
+package top.lanscarlos.vulpecula.module.dispatcher.pipeline.player
+
+import org.bukkit.event.entity.EntityDamageByEntityEvent
+import taboolib.library.configuration.ConfigurationSection
+import top.lanscarlos.vulpecula.module.dispatcher.pipeline.AutoRegistered
+
+/**
+ * Vulpecula
+ * top.lanscarlos.vulpecula.module.dispatcher.pipeline.player
+ *
+ * @author Lanscarlos
+ * @since 2025/7/29
+ */
+@AutoRegistered
+class PlayerDamageByEntityEventPipeline(clazz: Class<*>, config: ConfigurationSection) : VirtualPlayerEventPipeline<EntityDamageByEntityEvent>(clazz, config)
