1695db22885192eb5661fd118bfeb106706ef1c2
Lanscarlos <lanscarlos@hotmail.com>
2025-09-09 15:36:13 +0800

    update

--- workflow-build/build.gradle.kts
+++ workflow-build/build.gradle.kts
@@ -5,7 +5,7 @@ taboolib {
 for (project in rootProject.subprojects.filter { it.depth == 1 && it.name.startsWith("plugin-") }) {
     project.tasks.register("cleanResources") {
         delete(project.layout.buildDirectory.dir("resources"))
-        val workspace = file(project.layout.buildDirectory.dir("resources/main/action")).also(File::mkdirs)
+        file(project.layout.buildDirectory.dir("resources/main/action")).also(File::mkdirs)
     }
     project.tasks.jar {
         archiveClassifier.set("")
