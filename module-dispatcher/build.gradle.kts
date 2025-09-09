ab5b6e9d9df8f5be7dc8401517a06c05ba1e5591
Lanscarlos <lanscarlos@hotmail.com>
2025-09-09 15:40:00 +0800

    update

--- module-dispatcher/build.gradle.kts
+++ module-dispatcher/build.gradle.kts
@@ -10,7 +10,7 @@ dependencies {
     compileOnly(project(":module-bacikal"))
     compileOnly(project(":module-script"))
     compileOnly("ink.ptms.core:v12004:12004:mapped")
-    compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-20241030.192207-176")
 
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
